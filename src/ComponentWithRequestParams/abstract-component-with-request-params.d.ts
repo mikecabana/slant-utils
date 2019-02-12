fa524ad547ccb12e0da882976f5105fbf7e0f7d8
Michael Cabana <m.cabana@ieg-america.com>
2019-02-12 15:02:25 -0500

    Setup Package

--- src/ComponentWithRequestParams/abstract-component-with-request-params.d.ts
+++ src/ComponentWithRequestParams/abstract-component-with-request-params.d.ts
@@ -3,67 +3,66 @@ import { Observer } from 'rxjs';
 import { RequestParams } from './request-params.d';
 
 export abstract class AbstractComponentWithRequestParams {
-    /**
-     * Global record of the combined request params.
-     * This is what should get passed to Angular's HttpParams.
-     *
-     * @abstract
-     * @type {GlobalParams}
-     * @memberof AbstractComponentWithRequestParams
-     */
-    abstract globalParams: RequestParams;
+  /**
+   * Global record of the combined request params.
+   * This is what should get passed to Angular's HttpParams.
+   *
+   * @abstract
+   * @type {GlobalParams}
+   * @memberof AbstractComponentWithRequestParams
+   */
+  abstract globalParams: RequestParams;
 
-    /**
-     * Global record of the request params for this component.
-     * Populated via `registerRequestParams`.
-     * An observer will be registered to each of these via `subscribeToRegisteredParams` 
-     * so when the param changes, an update can occur.
-     *
-     * @abstract
-     * @type {Array<AbstractRequestParams<any>>}
-     * @memberof AbstractComponentWithRequestParams
-     */
-    abstract requestParams: Array<AbstractRequestParams<any>>;
+  /**
+   * Global record of the request params for this component.
+   * Populated via `registerRequestParams`.
+   * An observer will be registered to each of these via `subscribeToRegisteredParams`
+   * so when the param changes, an update can occur.
+   *
+   * @abstract
+   * @type {Array<AbstractRequestParams<any>>}
+   * @memberof AbstractComponentWithRequestParams
+   */
+  abstract requestParams: Array<AbstractRequestParams<any>>;
 
-    /**
-     * Observer to be used to subscribe to each request param.
-     * Use as the argument for `subscribeToRegisteredParams(observer: Observer<RequestParams>)`
-     *
-     * @abstract
-     * @type {Observer<RequestParams>}
-     * @memberof AbstractComponentWithRequestParams
-     */
-    abstract paramsObserver: Observer<RequestParams>;
+  /**
+   * Observer to be used to subscribe to each request param.
+   * Use as the argument for `subscribeToRegisteredParams(observer: Observer<RequestParams>)`
+   *
+   * @abstract
+   * @type {Observer<RequestParams>}
+   * @memberof AbstractComponentWithRequestParams
+   */
+  abstract paramsObserver: Observer<RequestParams>;
 
-    /**
-     * Iterate through `requestParams` and attach an observer by subscribing to each `AbstractRequestParam<T>`.
-     *
-     * @abstract
-     * @param {Observer<RequestParams>} observer
-     * @memberof AbstractComponentWithRequestParams
-     */
-    abstract subscribeToRegisteredParams(observer: Observer<RequestParams>): void;
+  /**
+   * Iterate through `requestParams` and attach an observer by subscribing to each `AbstractRequestParam<T>`.
+   *
+   * @abstract
+   * @param {Observer<RequestParams>} observer
+   * @memberof AbstractComponentWithRequestParams
+   */
+  abstract subscribeToRegisteredParams(observer: Observer<RequestParams>): void;
 
-    /**
-     * Add an `AbstractRequestParam<T>` to the global list of params `requestParams`.
-     *
-     * @abstract
-     * @param {Array<AbstractRequestParams<any>>} requestParams
-     * @memberof AbstractComponentWithRequestParams
-     */
-    abstract registerRequestParams(requestParamsForRegistering: Array<AbstractRequestParams<any>>): void;
+  /**
+   * Add an `AbstractRequestParam<T>` to the global list of params `requestParams`.
+   *
+   * @abstract
+   * @param {Array<AbstractRequestParams<any>>} requestParams
+   * @memberof AbstractComponentWithRequestParams
+   */
+  abstract registerRequestParams(requestParamsForRegistering: Array<AbstractRequestParams<any>>): void;
 
-    
-    /**
-     * Find the concrete request params in the list of registered request params and return it.
-     * Pass a `new ` instance to type check and find the right object i.e. `new SortRequestParams()`.
-     * Possible implementation can be `return this.requestParams.filter(requestParam => requestParam instanceof concreteRequestParams)[0];`.
-     * 
-     * @abstract
-     * @template T
-     * @param {() => T} concreteRequestParams
-     * @returns {AbstractRequestParams<any>}
-     * @memberof AbstractComponentWithRequestParams
-     */
-    abstract getRegisteredRequestParams<T>(concreteRequestParams: () => T): AbstractRequestParams<any>;
+  /**
+   * Find the concrete request params in the list of registered request params and return it.
+   * Pass a `new ` instance to type check and find the right object i.e. `new SortRequestParams()`.
+   * Possible implementation can be `return this.requestParams.filter(requestParam => requestParam instanceof concreteRequestParams)[0];`.
+   *
+   * @abstract
+   * @template T
+   * @param {() => T} concreteRequestParams
+   * @returns {AbstractRequestParams<any>}
+   * @memberof AbstractComponentWithRequestParams
+   */
+  abstract getRegisteredRequestParams<T>(concreteRequestParams: () => T): AbstractRequestParams<any>;
 }
--- src/ComponentWithRequestParams/abstract-request-params.d.ts
+++ src/ComponentWithRequestParams/abstract-request-params.d.ts
@@ -1,31 +1,31 @@
 import { Subject, Observable } from 'rxjs';
 
 export abstract class AbstractRequestParams<T> {
-    /**
-     * Global record of this request params values.
-     *
-     * @abstract
-     * @type {T}
-     * @memberof AbstractRequestParams
-     */
-    abstract params: T;
+  /**
+   * Global record of this request params values.
+   *
+   * @abstract
+   * @type {T}
+   * @memberof AbstractRequestParams
+   */
+  abstract params: T;
 
-    /**
-     * Global subject to emit this request params values.
-     *
-     * @abstract
-     * @type {Subject<T>}
-     * @memberof AbstractRequestParams
-     */
-    abstract subject: Subject<T>;
+  /**
+   * Global subject to emit this request params values.
+   *
+   * @abstract
+   * @type {Subject<T>}
+   * @memberof AbstractRequestParams
+   */
+  abstract subject: Subject<T>;
 
-    /**
-     * Get this request params Subject as an Observable by using the `pipe` operator.
-     * The `pipe` operator will allow us to apply Operator Functions to the Observable i.e. `distinctUntilChanged()`.
-     *
-     * @abstract
-     * @returns {Observable<T>}
-     * @memberof AbstractRequestParams
-     */
-    abstract getPipedParamsObservable(): Observable<T>;
+  /**
+   * Get this request params Subject as an Observable by using the `pipe` operator.
+   * The `pipe` operator will allow us to apply Operator Functions to the Observable i.e. `distinctUntilChanged()`.
+   *
+   * @abstract
+   * @returns {Observable<T>}
+   * @memberof AbstractRequestParams
+   */
+  abstract getPipedParamsObservable(): Observable<T>;
 }
